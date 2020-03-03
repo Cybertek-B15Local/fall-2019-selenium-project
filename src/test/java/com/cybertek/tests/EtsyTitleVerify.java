fcf9689d2d126f863e8e3fb0802ae45bf6405957
CybertekB15 <cybertekb15@CybertekSchools-iMac-2.local>
2020-03-03 16:53:33 -0500

    commit

---
+++ src/test/java/com/cybertek/tests/EtsyTitleVerify.java
@@ -0,0 +1,36 @@
+package com.cybertek.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EtsyTitleVerify {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        //go to etsy  --> data URL : https://www.etsy.com/
+
+        driver.get("https://www.etsy.com/");
+
+        //verify title
+        //expected vs actual
+
+        String expectedTitle = "etsy - Shop for handmade, vintage, custom, and unique gifts for everyone";
+
+        String actualTitle = driver.getTitle();
+
+        if(expectedTitle.equalsIgnoreCase(actualTitle)){
+            System.out.println("The test is Pass");
+        }else {
+            System.out.println("The test is Failed");
+            System.out.println(expectedTitle + " vs  " + actualTitle);
+        }
+
+        driver.close();
+
+
+    }
+}
---
+++ src/test/java/com/cybertek/tests/VrifyURLOfPracticeWeb.java
@@ -0,0 +1,15 @@
+package com.cybertek.tests;
+
+public class VrifyURLOfPracticeWeb {
+
+    public static void main(String[] args) {
+        /*
+        1. go to cybertek practice website
+                http://practice.cybertekschool.com/
+        2. verify the URL
+         */
+
+
+
+    }
+}
