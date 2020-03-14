e0bfc4f5c4069687574a6daec9bee89504a55f5a
cybertek <github@cybertekschool.com>
2020-03-14 11:52:53 -0400

    amazon test

--- src/test/java/com/cybertek/tests/day7_review/AmazonTests.java
+++ src/test/java/com/cybertek/tests/day7_review/AmazonTests.java
@@ -1,5 +1,11 @@
 package com.cybertek.tests.day7_review;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class AmazonTests {
     // main method to invoke the tests
     public static void main(String[] args) {
@@ -18,6 +24,21 @@ public class AmazonTests {
      * verify that product name is same in the product page
      */
     private static void nameMatchTest() {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("https://amazon.com");
+
+        String searchTerm = "disinfectant wipes";
+        WebElement searchInput = driver.findElement(By.id("twotabsearchtextbox"));
+        // OPTION 1: enter search term and hit ENTER button
+//        searchInput.sendKeys(searchTerm + Keys.ENTER);
+        // OPTION 2: Enter search term and click the search button
+        searchInput.sendKeys(searchTerm);
+        WebElement searchBtn = driver.findElement(By.className("nav-input"));
+        searchBtn.click();
+
+        WebElement firstResult = driver.findElement(By.xpath("//span[@class='a-size-base-plus a-color-base a-text-normal']"));
+        System.out.println(firstResult.getText());
+
 
     }
 
