5f616a0180342ed6b5767d2071b4aef9cd08af9c
cybertek <github@cybertekschool.com>
2020-03-14 10:53:24 -0400

    review 1

---
+++ src/test/java/com/cybertek/tests/day7_review/AmazonTests.java
@@ -0,0 +1,25 @@
+package com.cybertek.tests.day7_review;
+
+public class AmazonTests {
+    // main method to invoke the tests
+    public static void main(String[] args) {
+        nameMatchTest();
+
+    }
+
+    // each test will be a separate method
+
+    /**
+     * open browser
+     * go to amazon
+     * search for any item
+     * remember the name of the first result
+     * click on the first result
+     * verify that product name is same in the product page
+     */
+    private static void nameMatchTest() {
+
+    }
+
+
+}
