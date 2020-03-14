27a7e6e3bc630c84531e38a9938be451ac93662c
cybertek <github@cybertekschool.com>
2020-03-14 12:46:08 -0400

    amazon test

--- src/test/java/com/cybertek/tests/day7_review/AmazonTests.java
+++ src/test/java/com/cybertek/tests/day7_review/AmazonTests.java
@@ -10,7 +10,58 @@ public class AmazonTests {
     // main method to invoke the tests
     public static void main(String[] args) throws InterruptedException {
         nameMatchTest();
+        secondNameMatchTest();
 
+
+    }
+
+    // each test will be a separate method
+
+    /**
+     * open browser
+     * go to amazon
+     * search for any item
+     * remember the name of the second result
+     * click on the second result
+     * verify that product name is same in the product page
+     */
+    private static void secondNameMatchTest() throws InterruptedException {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("https://amazon.com");
+
+        String searchTerm = "disinfectant wipes";
+        WebElement searchInput = driver.findElement(By.id("twotabsearchtextbox"));
+        // OPTION 1: enter search term and hit ENTER button
+//        searchInput.sendKeys(searchTerm + Keys.ENTER);
+        // OPTION 2: Enter search term and click the search button
+        searchInput.sendKeys(searchTerm);
+        WebElement searchBtn = driver.findElement(By.className("nav-input"));
+        searchBtn.click();
+
+        Thread.sleep(2000);
+        // write xpath that captures second result
+        // CHANGE XPATH
+        WebElement firstResult = driver.findElement(By.xpath(""));
+        // it is prting the value
+        System.out.println(firstResult.getText());
+        // get the name of the second result
+        String expectedName = firstResult.getText();
+        // click on the second result
+        firstResult.click();
+        // creating a new variable
+        WebElement productName = driver.findElement(By.id("productTitle"));
+        String actualName = productName.getText();
+
+        System.out.println(actualName);
+
+        if (expectedName.equals(actualName)) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+            System.out.println("expectedName = " + expectedName);
+            System.out.println("actualName = " + actualName);
+        }
+        driver.quit();
     }
 
     // each test will be a separate method
@@ -37,13 +88,15 @@ public class AmazonTests {
         searchBtn.click();
 
         Thread.sleep(2000);
-
+        // write xpath that captures first result
         WebElement firstResult = driver.findElement(By.xpath("//span[@class='a-size-base-plus a-color-base a-text-normal']"));
+        // it is prting the value
         System.out.println(firstResult.getText());
+        // get the name of the first result
         String expectedName = firstResult.getText();
-
+        // click on the first result
         firstResult.click();
-
+        // creating a new variable
         WebElement productName = driver.findElement(By.id("productTitle"));
         String actualName = productName.getText();
 
@@ -58,6 +111,8 @@ public class AmazonTests {
         }
         driver.quit();
     }
+
+
 
 
 }
