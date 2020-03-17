a03e1477914a089fbee87ff063b429e13c00d3ec
cybertek <github@cybertekschool.com>
2020-03-17 18:59:14 -0400

    added testng dep

---
+++ src/test/java/com/cybertek/tests/day9_testng_intro/FirstTestNGTest.java
@@ -0,0 +1,11 @@
+package com.cybertek.tests.day9_testng_intro;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNGTest {
+
+    @Test
+    public void test1(){
+        System.out.println("test one");
+    }
+}
