3742a0642a97d451952180ae0d81bec61d02b207
cybertek <github@cybertekschool.com>
2020-03-17 21:26:23 -0400

    BeforeAndAfterTest.java

--- src/test/java/com/cybertek/tests/day9_testng_intro/BeforeAndAfterTest.java
+++ src/test/java/com/cybertek/tests/day9_testng_intro/BeforeAndAfterTest.java
@@ -1,12 +1,20 @@
 package com.cybertek.tests.day9_testng_intro;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Ignore;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BeforeAndAfterTest {
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("\tBEFORE CLASS");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("\tAFTER CLASS");
+
+    }
 
     @BeforeMethod
     public void beforeMethod(){
