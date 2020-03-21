4c3207d3e0776039cf3db63de15f2315c1414b3d
cybertek <github@cybertekschool.com>
2020-03-21 11:46:39 -0400

    CheckboxesTest.java

--- src/test/java/com/cybertek/tests/day10_webelement/CheckboxesTest.java
+++ src/test/java/com/cybertek/tests/day10_webelement/CheckboxesTest.java
@@ -1,9 +1,13 @@
 package com.cybertek.tests.day10_webelement;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class CheckboxesTest {
 
@@ -19,5 +23,17 @@ public class CheckboxesTest {
     @AfterMethod
     public void afterTest() {
         driver.quit();
+    }
+
+    @Test
+    public void test(){
+        WebElement one = driver.findElement(By.xpath("//input[1]"));
+        WebElement two = driver.findElement(By.xpath("//input[2]"));
+        // by default one is not checked, two is checked
+        System.out.println("is one selected: "+ one.isSelected());
+        System.out.println("is two selected: "+ two.isSelected());
+
+        Assert.assertFalse(one.isSelected());
+        Assert.assertTrue(two.isSelected());
     }
 }
