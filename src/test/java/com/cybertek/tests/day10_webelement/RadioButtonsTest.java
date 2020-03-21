9a51ee76ebd72da5c9c0251296a38da7bf96380d
cybertek <github@cybertekschool.com>
2020-03-21 10:34:55 -0400

    radio button example

---
+++ src/test/java/com/cybertek/tests/day10_webelement/RadioButtonsTest.java
@@ -0,0 +1,53 @@
+package com.cybertek.tests.day10_webelement;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RadioButtonsTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+
+    }
+
+    @AfterMethod
+    public void afterTest(){
+        driver.quit();
+    }
+
+    /*
+    go to http://practice.cybertekschool.com/radio_buttons
+    verify that blue is selected
+    red is not selected
+     */
+
+    @Test
+    public void test1(){
+        WebElement blue = driver.findElement(By.id("blue"));
+        // isSelected --> returns true if element is selected
+        System.out.println(blue.isSelected());
+
+        // verify is the radio button is selected
+        Assert.assertTrue(blue.isSelected());
+
+    }
+
+     /*
+    go to http://practice.cybertekschool.com/
+    click on red
+    verify that blue is not selected
+    red is  selected
+     */
+
+}
