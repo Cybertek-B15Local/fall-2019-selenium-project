b9d69a1d82d547b5beddbd74358b67fc4f723ba5
cybertek <github@cybertekschool.com>
2020-03-22 10:40:31 -0400

    select class

--- src/test/java/com/cybertek/tests/day11_select_actions/SelectClassTests.java
+++ src/test/java/com/cybertek/tests/day11_select_actions/SelectClassTests.java
@@ -1,9 +1,13 @@
 package com.cybertek.tests.day11_select_actions;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class SelectClassTests {
     WebDriver driver;
@@ -18,5 +22,18 @@ public class SelectClassTests {
     @AfterMethod
     public void afterTest() {
         driver.quit();
+    }
+
+    @Test
+    public void getSelectedOption(){
+        // FIND THE ELEMENT THAT HAS SELECT TAG
+        WebElement dropdown = driver.findElement(By.id("dropdown"));
+
+        // create a select class using the web element
+        Select dropdownList = new Select(dropdown);
+
+        // get the selected option
+        WebElement selectedOption = dropdownList.getFirstSelectedOption();
+
     }
 }
