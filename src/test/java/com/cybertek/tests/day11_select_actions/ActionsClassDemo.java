694cbfec04eb256ef2d1b6bcd3f7dbe42623283e
cybertek <github@cybertekschool.com>
2020-03-22 14:42:03 -0400

    select class

--- src/test/java/com/cybertek/tests/day11_select_actions/ActionsClassDemo.java
+++ src/test/java/com/cybertek/tests/day11_select_actions/ActionsClassDemo.java
@@ -37,4 +37,26 @@ public class ActionsClassDemo {
 
     }
 
+    @Test
+    public void hover2(){
+        driver.get("http://practice.cybertekschool.com/jqueryui/menu");
+        WebElement enabled = driver.findElement(By.id("ui-id-3"));
+        WebElement downloads = driver.findElement(By.id("ui-id-4"));
+        WebElement pdf = driver.findElement(By.id("ui-id-5"));
+
+        // move the mouse to enabled
+        // wait
+        // move the mouse to downloads
+        // wait
+        // click on PDF
+
+        actions.moveToElement(enabled).
+                pause(2000).
+                moveToElement(downloads).
+                pause(2000).
+                click(pdf).
+                build().perform();
+
+    }
+
 }
