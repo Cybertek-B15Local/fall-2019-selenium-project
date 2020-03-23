ebfc2501120661b702285c9a133df24e0278ecaa
cybertek <github@cybertekschool.com>
2020-03-23 17:14:43 -0400

    select list practice

--- src/test/java/com/cybertek/tests/office_hours2/SelectClassPracticeTests.java
+++ src/test/java/com/cybertek/tests/office_hours2/SelectClassPracticeTests.java
@@ -32,10 +32,6 @@ public class SelectClassPracticeTests {
     go to http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCellTable
     verify that table has dropdown with values Family, Friends, Coworkers, Businesses, Contacts
 
-    select option Coworkers
-    verify that that Coworkers is now selected
-    select options Contacts
-    verify that contacts is selected
      */
     @Test
     public void test() throws InterruptedException {
@@ -56,4 +52,23 @@ public class SelectClassPracticeTests {
         Assert.assertEquals(allOptionsStr, expectedOptions);
 
     }
+
+    /*
+     go to http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCellTable
+    select option Coworkers
+    verify that that Coworkers is now selected
+    select options Contacts
+    verify that contacts is selected
+     */
+    @Test
+    public void test2() throws InterruptedException {
+        driver.get("http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCellTable");
+        Thread.sleep(2000);
+        Select categories = new Select(driver.findElement(By.cssSelector("select[tabindex='-1']")));
+
+        // BREAK
+    }
+
+
+
 }
