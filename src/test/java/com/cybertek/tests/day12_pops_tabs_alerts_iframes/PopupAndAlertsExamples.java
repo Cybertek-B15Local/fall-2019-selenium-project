51d65918cc672d3f1c548b72f818b560a6ff33e2
cybertek <github@cybertekschool.com>
2020-03-24 18:55:23 -0400

    html pop up

---
+++ src/test/java/com/cybertek/tests/day12_pops_tabs_alerts_iframes/PopupAndAlertsExamples.java
@@ -0,0 +1,34 @@
+package com.cybertek.tests.day12_pops_tabs_alerts_iframes;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PopupAndAlertsExamples {
+
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.quit();
+    }
+
+    @Test
+    public void htmlPopup(){
+        driver.get("https://www.primefaces.org/showcase/ui/overlay/confirmDialog.xhtml");
+        WebElement button = driver.findElement(By.xpath("//span[text()='Destroy the World']"));
+        button.click();
+
+    }
+}
