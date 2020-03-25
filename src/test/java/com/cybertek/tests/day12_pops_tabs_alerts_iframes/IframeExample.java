035a876bcc3308a8e9fb783de059526819c37cc0
cybertek <github@cybertekschool.com>
2020-03-24 20:01:19 -0400

    IframeExample.java

--- src/test/java/com/cybertek/tests/day12_pops_tabs_alerts_iframes/IframeExample.java
+++ src/test/java/com/cybertek/tests/day12_pops_tabs_alerts_iframes/IframeExample.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day12_pops_tabs_alerts_iframes;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -22,7 +24,19 @@ public class IframeExample {
     }
 
     @Test
-    public void test(){
+    public void test() throws InterruptedException {
         driver.get("http://practice.cybertekschool.com/tinymce");
+
+        Thread.sleep(3000);
+
+        // we will switch to iframe.
+        // TODO switch by id/name
+        driver.switchTo().frame("mce_0_ifr");
+
+        WebElement textBox = driver.findElement(By.id("tinymce"));
+        textBox.click();
+        textBox.sendKeys("great.thanks");
+
+
     }
 }
