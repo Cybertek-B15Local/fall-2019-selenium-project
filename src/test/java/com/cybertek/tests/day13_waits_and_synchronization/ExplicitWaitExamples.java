d2c32a8c547f57e84359017287583e39c8101c3b
marufjont <marufjont@gmail.com>
2020-03-28 12:41:25 -0400

    waits

--- src/test/java/com/cybertek/tests/day13_waits_and_synchronization/ExplicitWaitExamples.java
+++ src/test/java/com/cybertek/tests/day13_waits_and_synchronization/ExplicitWaitExamples.java
@@ -4,6 +4,7 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class ExplicitWaitExamples {
 
@@ -11,7 +12,7 @@ public class ExplicitWaitExamples {
 
     @BeforeMethod
     public void setUp() {
-        driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("firefox");
 
     }
 
@@ -19,4 +20,17 @@ public class ExplicitWaitExamples {
     public void tearDown() {
         driver.quit();
     }
+
+    @Test
+    public void test1TitleTest(){
+        driver.get("https://google.com");
+
+        System.out.println(driver.getTitle());
+
+        driver.get("https://steam.com");
+
+        System.out.println(driver.getTitle());
+
+    }
+
 }
