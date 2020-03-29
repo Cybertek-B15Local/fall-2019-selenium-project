ca244fdccaf4215af1e9a727dee832089f29cff4
marufjont <marufjont@gmail.com>
2020-03-29 15:41:46 -0400

    driver class test base class, properties reader

--- src/test/java/com/cybertek/tests/day14_test_base_props_driver/FileUploadTests.java
+++ src/test/java/com/cybertek/tests/day14_test_base_props_driver/FileUploadTests.java
@@ -4,6 +4,7 @@ import com.cybertek.base.TestBase;
 import com.cybertek.utilities.ConfigurationReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FileUploadTests extends TestBase {
@@ -26,6 +27,12 @@ public class FileUploadTests extends TestBase {
         WebElement fileInput = driver.findElement(By.id("file-upload"));
 
         fileInput.sendKeys("/Users/marufjon/Desktop/my_file.txt");
+
+        WebElement submit = driver.findElement(By.id("file-submit"));
+        submit.click();
+
+        WebElement uploadedFile = driver.findElement(By.id("uploaded-files"));
+        Assert.assertEquals(uploadedFile.getText(), "my_file.txt");
 
     }
 }
