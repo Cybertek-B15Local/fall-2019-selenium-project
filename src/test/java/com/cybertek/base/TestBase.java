fd7c2d47366bc058b0a0b6ec70cbc128f6f8df98
marufjont <marufjont@gmail.com>
2020-03-29 11:47:33 -0400

    driver class test base class, properties reader

--- src/test/java/com/cybertek/base/TestBase.java
+++ src/test/java/com/cybertek/base/TestBase.java
@@ -1,4 +1,21 @@
 package com.cybertek.base;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class TestBase {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.quit();
+    }
 }
+
