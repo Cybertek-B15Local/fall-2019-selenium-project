c956a6df8510220e7fb17e264402292170d8bf76
marufjont <marufjont@gmail.com>
2020-03-30 16:56:08 -0400

    etsy test

--- src/test/java/com/cybertek/tests/office_hours3/EtsyTests.java
+++ src/test/java/com/cybertek/tests/office_hours3/EtsyTests.java
@@ -3,12 +3,17 @@ package com.cybertek.tests.office_hours3;
 import com.cybertek.base.TestBase;
 import com.cybertek.utilities.WebDriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class EtsyTests extends TestBase {
     /*
     search for wooden spoon on etsy
+    verify title contains the searchterm
     verify that default shipt to location is Ship to
     verify Ship to countries include Australia
      */
@@ -16,6 +21,10 @@ public class EtsyTests extends TestBase {
     @Test
     public void verifyCountryList(){
         driver.get("https://etsy.com");
+        WebElement input = driver.findElement(By.id("global-enhancements-search-query"));
+        input.sendKeys("wooden spoon"+ Keys.ENTER);
 
+        String expectedTitle = "Wooden spoon | Etsy";
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
     }
 }
