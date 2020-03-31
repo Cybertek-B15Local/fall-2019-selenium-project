fde545dda66cc4b956198bfbc837e0bd8ef799db
marufjont <marufjont@gmail.com>
2020-03-31 19:19:21 -0400

    web tables

--- src/test/java/com/cybertek/tests/day15_webtables/WebTablesTests.java
+++ src/test/java/com/cybertek/tests/day15_webtables/WebTablesTests.java
@@ -1,11 +1,14 @@
 package com.cybertek.tests.day15_webtables;
 
 import com.cybertek.base.TestBase;
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.ConfigurationReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class WebTablesTests extends TestBase {
 
@@ -26,6 +29,15 @@ public class WebTablesTests extends TestBase {
     // GET ALL HEADERS
     @Test
     public void getHeaders(){
+        // get all headers in a single element
+        WebElement header = driver.findElement(By.tagName("thead"));
+        System.out.println(header.getText());
+
+        // get all headers in a list
+        List<WebElement> headers = driver.findElements(By.xpath("//table[@id='table1']//th"));
+        System.out.println(headers.size());
+
+        System.out.println(BrowserUtils.getElementsText(headers));
 
     }
 }
