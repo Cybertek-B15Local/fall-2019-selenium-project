97ab5a00d0be066a540fd5f80e491c4f712d7ea1
marufjont <marufjont@gmail.com>
2020-03-31 19:04:55 -0400

    web tables

--- src/test/java/com/cybertek/tests/day15_webtables/WebTablesTests.java
+++ src/test/java/com/cybertek/tests/day15_webtables/WebTablesTests.java
@@ -3,6 +3,7 @@ package com.cybertek.tests.day15_webtables;
 import com.cybertek.base.TestBase;
 import com.cybertek.utilities.ConfigurationReader;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class WebTablesTests extends TestBase {
@@ -12,7 +13,7 @@ public class WebTablesTests extends TestBase {
         String url = ConfigurationReader.getProperty("url");
         driver.get(url);
         driver.findElement(By.linkText("Sortable Data Tables")).click();
-
-
+        WebElement myTable = driver.findElement(By.id("table1"));
+        System.out.println(myTable.getText());
     }
 }
