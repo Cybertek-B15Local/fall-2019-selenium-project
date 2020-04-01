af18235f5ddc6a9d8065e38a24d06c7a4cf25b0d
marufjont <marufjont@gmail.com>
2020-03-31 21:30:41 -0400

    web tables

--- src/test/java/com/cybertek/tests/day15_webtables/WebTablesTests.java
+++ src/test/java/com/cybertek/tests/day15_webtables/WebTablesTests.java
@@ -13,22 +13,23 @@ import java.util.List;
 public class WebTablesTests extends TestBase {
 
     @BeforeMethod
-    public void setUpTest(){
+    public void setUpTest() {
         String url = ConfigurationReader.getProperty("url");
         driver.get(url);
         driver.findElement(By.linkText("Sortable Data Tables")).click();
 
     }
+
     // GET WHOLE TABLE
     @Test
-    public void printTable(){
+    public void printTable() {
         WebElement myTable = driver.findElement(By.id("table1"));
         System.out.println(myTable.getText());
     }
 
     // GET ALL HEADERS
     @Test
-    public void getHeaders(){
+    public void getHeaders() {
         // get all headers in a single element
         WebElement header = driver.findElement(By.tagName("thead"));
         System.out.println(header.getText());
@@ -43,24 +44,24 @@ public class WebTablesTests extends TestBase {
 
     // GET table size
     @Test
-    public void getTableSize(){
+    public void getTableSize() {
         // get number of cols
         List<WebElement> headers = driver.findElements(By.xpath("//table[@id='table1']//th"));
-        System.out.println("Number columns: "+headers.size());
+        System.out.println("Number columns: " + headers.size());
 
         // get number of rows including header
         List<WebElement> rowsWithHeader = driver.findElements(By.xpath("//table[@id='table1']//tr"));
-        System.out.println("Number of row with header: "+ rowsWithHeader.size());
+        System.out.println("Number of row with header: " + rowsWithHeader.size());
 
         // get number of rows without header
         List<WebElement> rowsWithOutHeader = driver.findElements(By.xpath("//table[@id='table1']/tbody/tr"));
-        System.out.println("Number of row without header: "+ rowsWithOutHeader.size());
+        System.out.println("Number of row without header: " + rowsWithOutHeader.size());
 
     }
 
     // GET SINGLE ROW BY INDEX
     @Test
-    public void getSingleRowByIndex(){
+    public void getSingleRowByIndex() {
         // get the first row (in the body)
         WebElement row = driver.findElement(By.xpath("//table[@id='table1']/tbody/tr[1]"));
         System.out.println(row.getText());
@@ -71,15 +72,16 @@ public class WebTablesTests extends TestBase {
         System.out.println(row.getText());
 
     }
+
     // getTableRowXpath based on index
-    public static String getTableRowXpath(int index){
-        String xpath = "//table[@id='table1']/tbody/tr["+index+"]";
+    public static String getTableRowXpath(int index) {
+        String xpath = "//table[@id='table1']/tbody/tr[" + index + "]";
         return xpath;
     }
 
     // GET SINGLE CELL BASED ON INDEX
     @Test
-    public void getSingleBasedByIndex(){
+    public void getSingleBasedByIndex() {
         WebElement cell = driver.findElement(By.xpath("//table[@id='table1']/tbody/tr[1]/td[1]"));
         System.out.println(cell.getText());
 
@@ -88,9 +90,38 @@ public class WebTablesTests extends TestBase {
         System.out.println(cell.getText());
     }
 
-    public static String getCellXpathByIndex (int row,int column){
-        String xpath = "//table[@id='table1']/tbody/tr["+row+"]/td["+column+"]";
+    public static String getCellXpathByIndex(int row, int column) {
+        String xpath = "//table[@id='table1']/tbody/tr[" + row + "]/td[" + column + "]";
         return xpath;
+    }
+
+
+    // GO THROUGH THE TABLE USING LOOPS AND GET ALL VALUES
+    @Test
+    public void iterateTheTableUsingLoops() {
+        // get the number of rows
+        int rowCount = getRowCount();
+        // get the number of cols
+        int colCount = getColCount();
+
+        for (int i = 1; i <= rowCount; i++) {
+            for (int j = 1; j <= colCount; j++) {
+                String xpath = getCellXpathByIndex(i, j);
+                WebElement cell = driver.findElement(By.xpath(xpath));
+                System.out.println(cell.getText());
+            }
+        }
+    }
+
+    //  returns number of rows excluding the table headers
+    public int getRowCount() {
+        return driver.findElements(By.xpath("//table[@id='table1']/tbody/tr")).size();
+    }
+
+    // return number of rows
+    public int getColCount() {
+        return driver.findElements(By.xpath("//table[@id='table1']//th")).size();
+
     }
 
 }
