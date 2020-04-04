1f0c7adcb4e3adac8e47e18a68a6659ee82ca804
marufjont <marufjont@gmail.com>
2020-04-04 14:15:24 -0400

    page object model

---
+++ src/test/java/com/cybertek/pages/DashboardPage.java
@@ -0,0 +1,16 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage {
+    public DashboardPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(className = "oro-subtitle")
+    public WebElement pageHader;
+}
---
+++ src/test/java/com/cybertek/tests/day16_page_object_model/PageHeadersTests.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests.day16_page_object_model;
+
+import com.cybertek.base.TestBase;
+
+public class PageHeadersTests extends TestBase {
+    // go to vyrack
+    // login as driver
+    // verify  header is "Dashboard"
+
+
+//    go to vyrack
+    // go to Fleet vehicles
+    // login as driver
+    // verify  header is "All Cars"
+
+}
