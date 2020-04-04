4c81c973fe14fefc4a8467d7fc7bba9503b1da85
marufjont <marufjont@gmail.com>
2020-04-04 10:37:28 -0400

    page object model

--- src/test/java/com/cybertek/pages/LoginPage.java
+++ src/test/java/com/cybertek/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package com.cybertek.pages;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
@@ -9,6 +11,16 @@ public class LoginPage {
         // from selenium and pass a webdriver object and this class as param
         PageFactory.initElements(Driver.getDriver(), this);
 
-
     }
+
+    // FindBy --> we provide the locator for webelement
+    @FindBy(id = "prependedInput")
+    public WebElement username;
+
+    @FindBy(id = "prependedInput2")
+    public WebElement password;
+
+    @FindBy(name = "_submit")
+    public WebElement logIn;
+
 }
