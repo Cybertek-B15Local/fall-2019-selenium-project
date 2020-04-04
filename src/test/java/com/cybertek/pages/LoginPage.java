24eb86b109529b470099648c45724f23102d326d
marufjont <marufjont@gmail.com>
2020-04-04 11:43:17 -0400

    page object model

--- src/test/java/com/cybertek/pages/LoginPage.java
+++ src/test/java/com/cybertek/pages/LoginPage.java
@@ -1,9 +1,12 @@
 package com.cybertek.pages;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class LoginPage {
     public LoginPage() {
@@ -25,4 +28,9 @@ public class LoginPage {
 
     @FindBy(css = ".alert-error>div")
     public WebElement errorMessage;
+
+    public WebElement getUsername(){
+        return Driver.getDriver().findElement(By.id("asdfasdf"));
+    }
+
 }
--- src/test/java/com/cybertek/tests/day16_page_object_model/NegativeLoginTests.java
+++ src/test/java/com/cybertek/tests/day16_page_object_model/NegativeLoginTests.java
@@ -6,24 +6,42 @@ import com.cybertek.utilities.ConfigurationReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class NegativeLoginTests extends TestBase {
 
-    @Test
-    public void wrongUsernameTest(){
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void setUpTests() {
         driver.get(ConfigurationReader.getProperty("vytrack_url"));
+        loginPage = new LoginPage();
 
-        LoginPage loginPage = new LoginPage();
+    }
 
+    @Test
+    public void wrongUsernameTest() {
         loginPage.username.sendKeys("user30000");
         loginPage.password.sendKeys("UserUser123");
         loginPage.logIn.click();
 
         String actual = loginPage.errorMessage.getText();
 
+        Assert.assertTrue(loginPage.errorMessage.isDisplayed());
         Assert.assertEquals(actual, "Invalid user name or password.");
     }
 
+    @Test
+    public void wrongPasswordTest() {
+        loginPage.username.sendKeys("SalesManager110");
+        loginPage.password.sendKeys("LC");
+        loginPage.logIn.click();
+
+        String actual = loginPage.errorMessage.getText();
+        Assert.assertEquals(actual, "Invalid user name or password.");
+        Assert.assertTrue(loginPage.errorMessage.isDisplayed());
+
+    }
 
 }
