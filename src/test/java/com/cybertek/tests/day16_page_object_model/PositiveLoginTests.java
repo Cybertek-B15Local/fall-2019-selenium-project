8c9d07112b74950d991e03290d7ef1653c839004
marufjont <marufjont@gmail.com>
2020-04-04 12:36:36 -0400

    page object model

--- src/test/java/com/cybertek/tests/day16_page_object_model/PositiveLoginTests.java
+++ src/test/java/com/cybertek/tests/day16_page_object_model/PositiveLoginTests.java
@@ -5,7 +5,6 @@ import com.cybertek.pages.LoginPage;
 import com.cybertek.utilities.ConfigurationReader;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -43,6 +42,17 @@ public class PositiveLoginTests extends TestBase {
         wait.until(ExpectedConditions.titleIs("Dashboard"));
         String actualTitle = driver.getTitle();
         assertEquals(actualTitle, "Dashboard");
+
+    }
+
+    @Test
+    public void loginAsStoreManager(){
+        String username = ConfigurationReader.getProperty("store_manager_username");
+        String password = ConfigurationReader.getProperty("store_manager_password");
+        loginPage.login(username, password);
+
+        wait.until(ExpectedConditions.titleIs("Dashboard"));
+        assertEquals(driver.getTitle(), "Dashboard");
 
     }
 
