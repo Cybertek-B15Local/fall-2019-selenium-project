4b88db5c2eee2edba367e5d3845b98488cb6c7dd
marufjont <marufjont@gmail.com>
2020-04-05 10:44:39 -0400

    ddt intro

--- src/test/java/com/cybertek/tests/day17_ddt_dataprovider_pom2/UserInfoDataDrivenTests.java
+++ src/test/java/com/cybertek/tests/day17_ddt_dataprovider_pom2/UserInfoDataDrivenTests.java
@@ -6,19 +6,22 @@ import org.testng.annotations.Test;
 public class UserInfoDataDrivenTests {
 
     @Test(dataProvider = "users")
-    public void test(String username, String password) {
+    public void test(String username, String password, int number) {
         System.out.println("opening application");
         System.out.println("login as: " + username);
+        System.out.println("number is: " + number);
         System.out.println("verify info");
     }
-
 
 
     @DataProvider(name = "users")
     public Object[][] getUsers() {
         return new Object[][]
                 {
-                        {"user1", "UserUser123"},
+                        {"user1", "UserUser123", 25},
+                        {"salesmanager355", "UserUser123", 26},
+                        {"storemanager355", "UserUser123", 236},
+                        {"salesmanager4000", "UserUser123", 262},
                 };
     }
 
