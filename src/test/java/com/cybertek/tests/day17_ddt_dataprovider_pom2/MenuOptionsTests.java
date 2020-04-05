a269eb952166535d897d0b931aa8f63a188e587d
marufjont <marufjont@gmail.com>
2020-04-05 12:51:07 -0400

    ddt with vytrack

--- src/test/java/com/cybertek/tests/day17_ddt_dataprovider_pom2/MenuOptionsTests.java
+++ src/test/java/com/cybertek/tests/day17_ddt_dataprovider_pom2/MenuOptionsTests.java
@@ -2,7 +2,10 @@ package com.cybertek.tests.day17_ddt_dataprovider_pom2;
 
 import com.cybertek.base.VytrackTestBase;
 import com.cybertek.utilities.ConfigurationReader;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class MenuOptionsTests extends VytrackTestBase {
 
@@ -11,8 +14,9 @@ public class MenuOptionsTests extends VytrackTestBase {
     public void test(){
         loginPage.login(ConfigurationReader.getProperty("driver_username"),
                 ConfigurationReader.getProperty("driver_password"));
-
         dashboardPage.changeMenu("Activities", "Calendar Events");
+        wait.until(ExpectedConditions.titleIs("Calendar Events - Activities"));
+        assertEquals(driver.getTitle(), "Calendar Events - Activities");
 
     }
 }
