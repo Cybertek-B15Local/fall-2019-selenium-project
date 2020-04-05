2b3761afaf249f3822b17c153ded628b615b0b30
marufjont <marufjont@gmail.com>
2020-04-05 14:48:10 -0400

    ddt with vytrack

---
+++ src/test/java/com/cybertek/pages/CreateCalendarEventsPage.java
@@ -0,0 +1,19 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CreateCalendarEventsPage {
+    public CreateCalendarEventsPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(name = "oro_calendar_event_form[allDay]")
+    public WebElement allDayEventCheckbox;
+
+    @FindBy(css = "input[id^='recurrence-repeat-view']")
+    public WebElement repeatCheckbox;
+
+}
---
+++ src/test/java/com/cybertek/tests/day17_ddt_dataprovider_pom2/CreateCalendarEventTests.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests.day17_ddt_dataprovider_pom2;
+
+import com.cybertek.base.TestBase;
+import com.cybertek.base.VytrackTestBase;
+import com.cybertek.utilities.ConfigurationReader;
+import org.testng.annotations.Test;
+
+public class CreateCalendarEventTests extends VytrackTestBase {
+
+    @Test
+    public void allDayEventCheckBoxTest(){
+        loginPage.login(ConfigurationReader.getProperty("driver_username"),
+                ConfigurationReader.getProperty("driver_password"));
+        driver.get("https://qa3.vytrack.com/calendar/event/create");
+    }
+}
