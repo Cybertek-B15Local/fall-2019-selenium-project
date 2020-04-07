6293f67d9c4c362330d0a37a77dbac9f1903b1a9
marufjont <marufjont@gmail.com>
2020-04-07 19:55:48 -0400

    downgraded extent version

--- src/test/java/com/cybertek/tests/day18_html_report/ExtentReportExample.java
+++ src/test/java/com/cybertek/tests/day18_html_report/ExtentReportExample.java
@@ -3,7 +3,6 @@ package com.cybertek.tests.day18_html_report;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.ExtentHtmlReporterConfiguration;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -27,7 +26,7 @@ public class ExtentReportExample {
         // i need to point to the location where report will be create
         // System.getProperty("user.dir") --> gets my current folder
         // + "test-output/report.html" --> create folder test-output and in it create file report.html
-        String path = System.getProperty("user.dir")+"test-output/report.html";
+        String path = System.getProperty("user.dir")+"/test-output/report.html";
         // initialize the html reporter with the given path
         htmlReporter = new ExtentHtmlReporter(path);
         // set name for the report
@@ -46,7 +45,7 @@ public class ExtentReportExample {
         test.info("Opening browser");
         // driver = new Chromedriver
         test.info("going to website");
-        // driver.get(gogole.com)
+        // driver.get(goole.com)
         test.info("verifying title");
         // assertEquals(google, google)
         test.pass("VYT-123 Title verification test");
