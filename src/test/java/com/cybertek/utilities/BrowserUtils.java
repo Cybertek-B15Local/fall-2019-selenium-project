1a0e392eadf66874dee36774af978327c5e98f8c
marufjont <marufjont@gmail.com>
2020-04-07 20:45:09 -0400

    downgraded extent version

--- src/test/java/com/cybertek/utilities/BrowserUtils.java
+++ src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -85,14 +85,14 @@ public class BrowserUtils {
      * @param name
      * take a name of a test and returns a path to screenshot takes
      */
-    public static String getScreenshot() throws IOException {
+    public static String getScreenshot(String name) throws IOException {
         // name the screenshot with the current date time to avoid duplicate name
         String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
         // TakesScreenshot ---> interface from selenium which takes screenshots
         TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
         File source = ts.getScreenshotAs(OutputType.FILE);
         // full path to the screenshot location
-        String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + date + ".png";
+        String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
         File finalDestination = new File(target);
         // save the screenshot to the path given
         FileUtils.copyFile(source, finalDestination);
