b48467f0d78b0bed8c2d7d073e92bad17fb2731e
marufjont <marufjont@gmail.com>
2020-04-07 20:42:11 -0400

    downgraded extent version

--- src/test/java/com/cybertek/utilities/BrowserUtils.java
+++ src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
@@ -79,38 +80,22 @@ public class BrowserUtils {
         ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
     }
 
-    /**
-     * @param name screenshot name
-     * @return path to the screenshot
+    /*
+     * takes screenshot
+     * @param name
+     * take a name of a test and returns a path to screenshot takes
      */
-    public static String getScreenshot(String name) {
-        //adding date and time to screenshot name, to make screenshot unique
-        name = new Date().toString().replace(" ", "_").replace(":", "-") + "_" + name;
-        //where we gonna store a screenshot
-        String path = "";
-
-        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
-            path = System.getProperty("user.dir") + "/test-output/screenshots/" + name + ".png";
-        } else {
-            path = System.getProperty("user.dir") + "\\test-output\\screenshots\\" + name + ".png";
-        }
-
-        System.out.println("OS name: " + System.getProperty("os.name"));
-        System.out.println("Screenshot is here: " + path);
-        //since our reference type is a WebDriver
-        //we cannot see methods from TakesScreenshot interface
-        //that's why do casting
-        TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
-        //take screenshot of web browser, and save it as a file
-        File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
-        //where screenshot will be saved
-        File destination = new File(path);
-        try {
-            //copy file to the previously specified location
-            FileUtils.copyFile(source, destination);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return path;
+    public static String getScreenshot(String name) throws IOException {
+        // name the screenshot with the current date time to avoid duplicate name
+        String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        // TakesScreenshot ---> interface from selenium which takes screenshots
+        TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        // full path to the screenshot location
+        String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
+        File finalDestination = new File(target);
+        // save the screenshot to the path given
+        FileUtils.copyFile(source, finalDestination);
+        return target;
     }
 }
