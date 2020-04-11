a63d29d77ed0327ef9bea8917f419ee01fbae4e6
marufjont <marufjont@gmail.com>
2020-04-11 16:43:44 -0400

    excel write

--- src/test/java/com/cybertek/tests/day19_excel_io/ExcelUtilityDemo.java
+++ src/test/java/com/cybertek/tests/day19_excel_io/ExcelUtilityDemo.java
@@ -1,11 +1,24 @@
 package com.cybertek.tests.day19_excel_io;
 
+
+import com.cybertek.utilities.ExcelUtil;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.Map;
+
 public class ExcelUtilityDemo {
+    private String filePath = "src/test/resources/Vytrack testusers.xlsx";
+    private String sheetName = "QA3-short";
 
     @Test
-    public void test(){
+    public void test() {
 
+        ExcelUtil qa3Short = new ExcelUtil(filePath, sheetName);
+        System.out.println(qa3Short.getColumnsNames());
+
+        String data [][] = qa3Short.getDataArray();
+        List<Map<String, String>> dataList = qa3Short.getDataList();
+        System.out.println(dataList);
     }
 }
