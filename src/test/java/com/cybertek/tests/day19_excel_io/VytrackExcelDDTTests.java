bf1e3a842a780fae1d888ace47197584a06e636a
marufjont <marufjont@gmail.com>
2020-04-11 14:43:53 -0400

    excel read

--- src/test/java/com/cybertek/tests/day19_excel_io/VytrackExcelDDTTests.java
+++ src/test/java/com/cybertek/tests/day19_excel_io/VytrackExcelDDTTests.java
@@ -1,9 +1,7 @@
 package com.cybertek.tests.day19_excel_io;
 
 import com.cybertek.base.VytrackTestBase;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -39,11 +37,24 @@ public class VytrackExcelDDTTests extends VytrackTestBase {
         Workbook workbook = WorkbookFactory.create(file);
         Sheet workSheet = workbook.getSheet("QA3-short");
 
-        int rowCount = workSheet.getPhysicalNumberOfRows()-1;
-        int colCount = workSheet.getRow(0).getLastCellNum()-1;
-
+        int rowCount = workSheet.getPhysicalNumberOfRows() - 1;
+        int colCount = workSheet.getRow(0).getLastCellNum() - 1;
+        // 2d array that willl contain the excel data
         Object[][] data = new Object[rowCount][colCount];
+        // iterate the worksheet.
+        // i =1 --> start with second row
+        // i <= rowCount  --> including the last row
+        for (int i = 1; i <= rowCount; i++) {
+            // get the row current index
+            Row row = workSheet.getRow(i);
+            // iterate the inner loop
+            for (int j = 0; j < colCount; j++) {
+                // get each cell from the row
+                Cell cell = row.getCell(j);
+                data[i - 1][j] = cell.toString();
+            }
+        }
 
-        return null;
+        return data;
     }
 }
