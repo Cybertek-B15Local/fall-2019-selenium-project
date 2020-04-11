bb3a1848c456614f9e388dd743026087957315aa
marufjont <marufjont@gmail.com>
2020-04-11 15:34:01 -0400

    excel write

---
+++ src/test/java/com/cybertek/tests/day19_excel_io/ExcelWriteExample.java
@@ -0,0 +1,50 @@
+package com.cybertek.tests.day19_excel_io;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ExcelWriteExample {
+
+    // path to the location of the excel file.
+    private String filePath = "src/test/resources/Vytrack testusers.xlsx";
+
+    // creates connection between jvm and the file.
+    private FileInputStream fileInputStream;
+
+    // represents the excel file
+    private Workbook workbook;
+
+    // represents the sheet that contains the actual data
+    private Sheet workSheet;
+
+    @BeforeMethod
+    public void setUp() throws IOException {
+        // initialize the input stream by giving the file location
+        fileInputStream = new FileInputStream(filePath);
+
+        // create the workbook the input stream
+        // WorkbookFactory.create --> this is used to create workbook from inputstream
+        workbook = WorkbookFactory.create(fileInputStream);
+
+        // workbook.getSheetAt --> get the sheet from excel based on 0 based count
+//        workSheet = workbook.getSheetAt(5);
+
+        // workbook.getSheet --> get sheet using name
+        workSheet = workbook.getSheet("QA3-short");
+    }
+
+
+    @AfterMethod
+    public void tearDown() throws IOException {
+        workbook.close();
+        fileInputStream.close();
+    }
+
+}
