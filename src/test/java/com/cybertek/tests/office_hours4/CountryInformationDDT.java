294af5b085d8db043fde92e6d675587ebdec66b7
marufjont <marufjont@gmail.com>
2020-04-13 17:42:12 -0400

    country test

--- src/test/java/com/cybertek/tests/office_hours4/CountryInformationDDT.java
+++ src/test/java/com/cybertek/tests/office_hours4/CountryInformationDDT.java
@@ -1,8 +1,10 @@
 package com.cybertek.tests.office_hours4;
 
 import com.cybertek.utilities.Driver;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -10,6 +12,7 @@ import org.testng.annotations.Test;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CountryInformationDDT {
 
@@ -21,15 +24,32 @@ public class CountryInformationDDT {
 
 
     @BeforeMethod
-    public void setUp() throws FileNotFoundException {
+    public void setUp() throws IOException {
 //        driver = Driver.getDriver();
 
         fileInputStream = new FileInputStream("src/test/resources/Countries.xlsx");
-
+        workbook = WorkbookFactory.create(fileInputStream);
+        workSheet = workbook.getSheet("Countries");
     }
 
     @Test
-    public void test(){
+    public void test() {
+        int count = workSheet.getPhysicalNumberOfRows();
+        System.out.println("count = " + count);
+
+        for (int i = 1; i < count; i++) {
+            Row currentRow = workSheet.getRow(i);
+            String execute = currentRow.getCell(0).toString();
+            System.out.println("execute = " + execute);
+            if (execute.equals("y")) {
+                // execute the test
+                String country = currentRow.getCell(1).toString();
+                String capital = currentRow.getCell(2).toString();
+            } else {
+                // skip
+                continue;
+            }
+        }
 
     }
 }
